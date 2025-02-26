d257655ee6e267e930d89ef6c0517a4e8ad91648
Bojan Antonovic <bojan@antonovic.ch>
2025-02-26 22:17:53 +0100

    re-introduced app.routes.ts to align it with the conventions

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {provideZoneChangeDetection} from '@angular/core';
-import {provideRouter, Routes} from '@angular/router';
-import {FirstComponent} from './app/first/first.component';
-import {SecondComponent} from './app/second/second.component';
+import {provideRouter} from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'first-component',
-    component: FirstComponent
-  },
-  {
-    path: 'second-component',
-    component: SecondComponent
-  },
-];
+import {routes} from './app/app.routes';
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
